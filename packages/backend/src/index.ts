bf639c6e8839f23e7e5da98ff8511ff7362e8c8f
Ryan Claussen <ryan.claussen@scotiabank.com>
2025-06-13 14:05:22 -0500

    add github organization

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -54,4 +54,7 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+// GitHub Organization
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
+
 backend.start();
